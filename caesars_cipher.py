e700a71e62172b420f184df8ed72b155fea174a1
Vitaly Penzev <vspenzev@eleron.org>
2024-11-21 15:27:30 +0300

    add_class

---
+++ caesars_cipher.py
@@ -0,0 +1,12 @@
+class CaesarsCipher:
+    def __init__(self):
+        self.code_symbols = ('''ABCDEFGHIJKLMNOPQRSTUVWXYZ
+                             abcdefghijklmnopqrstuvwxyz1234567890 !?.''')
+
+    def decrypt(self, message_decrypt):
+        pass
+
+    def encrypt(self, message_encrypt):
+        pass
+
+if __name__ == '__main__':
