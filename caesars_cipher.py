65d835427e89be209a18fb03f44907eb8c410412
Penzev Vitaly <satellite402@mail.ru>
2024-11-23 17:04:04 +0300

    to black

--- caesars_cipher.py
+++ caesars_cipher.py
@@ -1,8 +1,13 @@
 import enchant
 
+
 class CaesarsCipher:
     def __init__(self):
-        self.symbols = ('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.')
+        self.symbols = (
+             "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+             "abcdefghijklmnopqrstuvwxyz"
+             "1234567890 !?."
+        )
 
     def decrypt(self, message):
         for n in range(len(self.symbols) + 1):
@@ -13,20 +18,19 @@ class CaesarsCipher:
                     index -= n
                     for index in self.symbols[index]:
                         self.__phrase.append(index)
-            decrypt_phrases = {n: ''.join(self.__phrase)}
+            decrypt_phrases = {n: "".join(self.__phrase)}
             for key in decrypt_phrases.values():
-                a = key.split(' ')
+                a = key.split(" ")
                 counter = 0
                 for x in a:
-                    if x != '':
+                    if x != "":
                         d = enchant.Dict("en_US")
-                        if d.check(x) == True:
+                        if d.check(x) is True:
                             counter += 1
                             if counter > 1:
-                                print(f'Ключ к шифру: {n}')
-                                print(f'Расшифрованная фраза: {' '.join(a)}')
+                                print(f"Ключ к шифру: {n}")
+                                print(f"Расшифрованная фраза: {' '.join(a)}")
                                 break
-
 
     def encrypt(self, message, key):
         self.__phrase = []
@@ -37,12 +41,12 @@ class CaesarsCipher:
                 if index > 65:
                     index = index - 66
                 for index in self.symbols[index]:
-                    self.__phrase .append(index)
-            encrypt_phrase = ''.join(self.__phrase )
-        print(f'Фраза зашифрована: {encrypt_phrase}')
+                    self.__phrase.append(index)
+            encrypt_phrase = "".join(self.__phrase)
+        print(f"Фраза зашифрована: {encrypt_phrase}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     phrase = CaesarsCipher()
-    phrase.encrypt('The password to my mailbox is fBIvqX5yjw', 22)
-    phrase.decrypt('p41S!wAAE .zSB S9GS9w58x FS5AS2XeD?tMG6E')
+    phrase.encrypt("The password to my mailbox is fBIvqX5yjw", 21)
+    phrase.decrypt("o3zR v..D0?yRA0R8FR8v47w0ER4.R1WdC!sLF5D")
